67c30ca5b4d7dc8985ce38d30bb6ee3e41d78b95
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-15 14:27:41 -0300

    add mutation endpoint

---
+++ src/routes/mutation/+server.ts
@@ -0,0 +1,16 @@
+import { GraphQL } from '$lib/graphql';
+import type { Data } from '$lib/stores';
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async ({ request }) => {
+	const data: WithRequiredProperty<Data, 'url'> = await request.json();
+
+	const { url, cache } = data;
+
+	const graphql = new GraphQL(url);
+
+	const schema = await graphql.schema('Mutation', cache.schema);
+
+	return json({ data: schema });
+};
