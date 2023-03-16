9240a15cb45aae9a279b62b0c73ff18a86d4bef2
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-16 11:30:21 -0300

    add Main component

---
+++ src/lib/components/Main.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type schemaQuery from '$lib/graphql/queries/schema';
+	import type typeQuery from '$lib/graphql/queries/type';
+	import Description from './Description.svelte';
+	import EnumValues from './EnumValues.svelte';
+	import Fields from './Fields.svelte';
+	import InputFields from './InputFields.svelte';
+	import PossibleTypes from './PossibleTypes.svelte';
+
+	export let schema: Exclude<schemaQuery | typeQuery, null> | undefined;
+</script>
+
+{#if schema && schema['__type']}
+	<Description description={schema['__type'].description} />
+
+	<EnumValues enumValues={schema['__type'].enumValues} />
+
+	<InputFields inputFields={schema['__type'].inputFields} />
+
+	<PossibleTypes possibleTypes={schema['__type'].possibleTypes} />
+
+	<Fields fields={schema['__type'].fields} />
+{/if}
