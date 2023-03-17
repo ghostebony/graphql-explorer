9c7b3ee3e3f5937d5953b66165a153058efb46d1
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-17 08:13:03 -0300

    add Description component

---
+++ src/lib/components/Description.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Fieldset from './Fieldset.svelte';
+
+	export let description: string | null | undefined;
+</script>
+
+{#if description}
+	<Fieldset legend="Description:" borderColor="#446d66">
+		<p style:white-space="pre-wrap">
+			{description}
+		</p>
+	</Fieldset>
+{/if}
