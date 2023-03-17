de0f65e1273cd4773b0810a866db94cd6e5f990d
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-17 08:13:27 -0300

    add EnumValues component

---
+++ src/lib/components/EnumValues.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { EnumValue as SchemaEnumValue } from '$lib/graphql/queries/schema';
+	import type { EnumValue as TypeEnumValue } from '$lib/graphql/queries/type';
+	import Description from './Description.svelte';
+	import Details from './Details.svelte';
+	import Fieldset from './Fieldset.svelte';
+
+	export let enumValues: SchemaEnumValue[] | TypeEnumValue[] | null | undefined;
+</script>
+
+{#if enumValues}
+	<Fieldset legend="Enum Values:" borderColor="#19bcaa">
+		{#each enumValues as { name, description }}
+			<Details summary={name}>
+				<Description {description} />
+			</Details>
+		{/each}
+	</Fieldset>
+{/if}
