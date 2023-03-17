ac99bed8245fadd0eeab8425a924e60d3997fb3d
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-17 08:15:16 -0300

    add PossibleTypes component

---
+++ src/lib/components/PossibleTypes.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Type as SchemaType } from '$lib/graphql/queries/schema';
+	import type { Type as TypeType } from '$lib/graphql/queries/type';
+	import Fieldset from './Fieldset.svelte';
+	import Type from './Type.svelte';
+
+	export let possibleTypes: SchemaType[] | TypeType[] | null | undefined;
+</script>
+
+{#if possibleTypes}
+	<Fieldset legend="Possible Types:" borderColor="#9a493b">
+		{#each possibleTypes as type}
+			<Type {type} />
+		{/each}
+	</Fieldset>
+{/if}
