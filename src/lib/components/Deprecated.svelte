1c7a2671fd2f93a08664311d9db285cb84a5adc8
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-20 11:44:12 -0300

    add Deprecated component

---
+++ src/lib/components/Deprecated.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let isDeprecated: boolean;
+	export let deprecationReason: string | null;
+</script>
+
+{#if isDeprecated && deprecationReason}
+	<p style:white-space="pre-wrap" style:color={isDeprecated ? 'red' : 'inherit'}>
+		{deprecationReason}
+	</p>
+{/if}
