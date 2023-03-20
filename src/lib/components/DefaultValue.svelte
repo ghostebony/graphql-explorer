a6a05a4dfdadb4ae1c6e7ce340a4aee855d75023
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-20 11:42:30 -0300

    add DefaultValue component

---
+++ src/lib/components/DefaultValue.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Fieldset from './Fieldset.svelte';
+
+	export let defaultValue: unknown | null;
+</script>
+
+{#if defaultValue}
+	<Fieldset legend="Default Value:" borderColor="#3a0e12">
+		<p>
+			{defaultValue}
+		</p>
+	</Fieldset>
+{/if}
