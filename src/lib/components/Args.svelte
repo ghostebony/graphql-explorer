d265a15684a4894ab5c33a394d315faeb69b01f2
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-20 11:42:54 -0300

    add Args component

---
+++ src/lib/components/Args.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Arg as SchemaArg } from '$lib/graphql/queries/schema';
+	import type { Arg as TypeArg } from '$lib/graphql/queries/type';
+	import Arg from './Arg.svelte';
+	import Fieldset from './Fieldset.svelte';
+
+	export let args: SchemaArg[] | TypeArg[];
+</script>
+
+{#if args.length > 0}
+	<Fieldset legend="Args:" borderColor="#2ad98c">
+		{#each args as arg}
+			<Arg {arg} />
+		{/each}
+	</Fieldset>
+{/if}
