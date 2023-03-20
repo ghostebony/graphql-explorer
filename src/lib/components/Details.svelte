0c840de716bb195459ee209cbbae28f202b5e790
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-20 11:44:30 -0300

    add Details component

---
+++ src/lib/components/Details.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let summary: string;
+
+	export let isDeprecated: boolean = false;
+
+	let open: boolean;
+</script>
+
+<details bind:open>
+	<summary style:cursor="pointer" style:color={isDeprecated ? 'red' : 'inherit'}>
+		{summary}
+	</summary>
+
+	{#if open}
+		<slot />
+	{/if}
+</details>
+
+<style>
+	details {
+		margin-top: 15px;
+	}
+</style>
