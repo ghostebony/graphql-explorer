abbf20513fcb3da0fcc0ad4df8634e0c9499b24e
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-20 07:23:51 -0300

    add Fieldset component

---
+++ src/lib/components/Fieldset.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let legend: string;
+
+	export let borderColor: string;
+</script>
+
+<fieldset style:border-color={borderColor}>
+	<legend>{legend}</legend>
+
+	<slot />
+</fieldset>
+
+<style>
+	fieldset {
+		margin-top: 25px;
+	}
+</style>
