bb752465fbc68ad4d35243cd6fa7c075de537ec3
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-20 11:43:47 -0300

    add Arg component

---
+++ src/lib/components/Arg.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Arg as SchemaArg } from '$lib/graphql/queries/schema';
+	import type { Arg as TypeArg } from '$lib/graphql/queries/type';
+	import DefaultValue from './DefaultValue.svelte';
+	import Description from './Description.svelte';
+	import Details from './Details.svelte';
+	import Type from './Type.svelte';
+
+	export let arg: SchemaArg | TypeArg;
+</script>
+
+<Details summary={arg.name}>
+	<Description description={arg.description} />
+
+	<DefaultValue defaultValue={arg.defaultValue} />
+
+	<Type type={arg.type} />
+</Details>
