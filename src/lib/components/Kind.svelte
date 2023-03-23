8b4564a627c5357c4c73c68911c5de328bbcf2fd
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-23 09:39:52 -0300

    add Kind component

---
+++ src/lib/components/Kind.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Fieldset from './Fieldset.svelte';
+
+	export let kind: string | null;
+</script>
+
+{#if kind}
+	<Fieldset legend="Kind:" borderColor="#d568dd">
+		<p>
+			{kind}
+		</p>
+	</Fieldset>
+{/if}
