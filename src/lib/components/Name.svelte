db30a44fa271288f4eaf6ace165465eb2cf95015
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-26 06:28:29 -0300

    add Name component

---
+++ src/lib/components/Name.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import ButtonFetchType from './ButtonFetchType.svelte';
+	import Fieldset from './Fieldset.svelte';
+
+	export let name: string | null;
+</script>
+
+{#if name}
+	<Fieldset legend="Name:" borderColor="#48c066">
+		<ButtonFetchType {name} />
+	</Fieldset>
+{/if}
