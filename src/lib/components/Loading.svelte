df60c1fc3617dff5beff8c53bbc40acd1f4fd46e
ghostebony <pedroamericolouzada@uol.com.br>
2023-03-31 10:43:07 -0300

    add Loading component

---
+++ src/lib/components/Loading.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let loading: boolean = false;
+</script>
+
+{#if loading}
+	<div style:margin-top="15px" style:margin-bottom="5px">
+		<span>Loading...</span>
+	</div>
+{/if}
